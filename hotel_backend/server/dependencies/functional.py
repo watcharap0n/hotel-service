ab44a9b4d70fecf066feb3e99799fe7e3928bf65
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2023-09-18 01:05:51 +0700

    functional.py

--- hotel_backend/server/dependencies/functional.py
+++ hotel_backend/server/dependencies/functional.py
@@ -1,88 +1,11 @@
-from fastapi import Security, status, HTTPException, Depends
+import os
+from typing import Union
 
-from ..routes.auth import get_current_user
-from ..schemas.employee import Employee
-from ..schemas.guest import Guest
-from ..schemas.user import User
+from fastapi import HTTPException, Header, status
 
 
-async def get_current_active_supervisor(
-        current_user: Employee = Security(get_current_user, scopes=['me', 'supervisor'])
-):
-    """
-    inactive user config this here:
-        - disabled
-    """
-    if current_user.disabled:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail='Inactive user')
-    if current_user.role is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Unauthorized')
-    if current_user.role != 'supervisor':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail='Not enough permissions')
-    return current_user
-
-
-async def get_current_active_employee(
-        current_user: Employee = Security(get_current_user, scopes=['me', 'employee'])
-):
-    """
-    inactive user config this here:
-        - disabled
-    """
-    if current_user.disabled:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail='Inactive user')
-    if current_user.role is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Unauthorized')
-    if current_user.role != 'supervisor' or current_user.role != 'employee':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail='Not enough permissions')
-    return current_user
-
-
-async def get_current_active_guest(
-        current_user: Guest = Security(get_current_user, scopes=['me', 'guest'])
-):
-    """
-    inactive user config this here:
-        - disabled
-    """
-    if current_user.disabled:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail='Inactive user')
-    if current_user.role is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Unauthorized')
-    if current_user.role != 'guest':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail='Not enough permissions')
-    return current_user
-
-
-async def get_current_active_group(
-        current_user: User = Security(get_current_user, scopes=['me', 'supervisor', 'employee', 'guest'])
-):
-    """
-    inactive user config this here:
-        - disabled
-    """
-    if current_user.disabled:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail='Inactive user')
-    if current_user.role is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail='Unauthorized')
-    return current_user
-
-
-def has_scope(required_role: str = Depends(get_current_user)):
-    def _has_scope(user: User = Depends(get_current_user)):
-        if user.role != required_role:
-            raise HTTPException(status_code=403, detail="Insufficient scope")
-        return user
-
-    return _has_scope
+async def x_api_token_authorize(x_api_token: Union[str, None] = Header(default=None)):
+    if not x_api_token:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='X-API-TOKEN header is bad request.')
+    if x_api_token != os.getenv('X_API_TOKEN', None):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='X-API-TOKEN header invalid')
